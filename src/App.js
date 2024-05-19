3c0db09fee4dac305dafc90e1f0d74c6adcc15b6
pedrosasouza70 <pedrosasouza70@gmail.com>
2024-05-18 21:46:36 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+
 import { Home } from './pages/Home';
 import axios from "axios";
+import logo from './logo.svg';
 
 function App() {
-  axios.get("https://pokeapi.co/api/v2/pokemon?limit=30&offset=0")
+  
   return (
     
     <Home/>
